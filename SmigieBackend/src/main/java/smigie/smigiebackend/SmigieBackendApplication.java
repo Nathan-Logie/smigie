107d21925457e10d713e6e387397be97e36da6df
Ross Smith <2652064s@student.gla.ac.uk>
2024-05-23 16:37:03 +0100

    Initial SpringBoot Backend Setup

---
+++ SmigieBackend/src/main/java/smigie/smigiebackend/SmigieBackendApplication.java
@@ -0,0 +1,13 @@
+package smigie.smigiebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmigieBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmigieBackendApplication.class, args);
+    }
+
+}
---
+++ SmigieBackend/src/test/java/smigie/smigiebackend/SmigieBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package smigie.smigiebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmigieBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
